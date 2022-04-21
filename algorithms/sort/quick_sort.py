8e8d0110df120ddd9c2db96af5eefe768747ec57
NijatTagizada <tagizada.nicat@gmail.com>
2022-04-21 15:32:30 +0400

    quick sort

--- algorithms/sort/quick_sort.py
+++ algorithms/sort/quick_sort.py
@@ -1,14 +1,14 @@
-def partition(array, low, high):
+def partition(array, left, right):
  
   # Choose the rightmost element as pivot
-  pivot = array[high]
+  pivot = array[right]
  
   # Pointer for greater element
-  i = low - 1
+  i = left - 1
  
   # Traverse through all elements
   # compare each element with pivot
-  for j in range(low, high):
+  for j in range(left, right):
     if array[j] <= pivot:
       # If element smaller than pivot is found
       # swap it with the greater element pointed by i
@@ -19,25 +19,25 @@ def partition(array, low, high):
   print(array)
  
   # Swap the pivot element with the greater element specified by i
-  (array[i + 1], array[high]) = (array[high], array[i + 1])
+  (array[i + 1], array[right]) = (array[right], array[i + 1])
  
   # Return the position from where partition is done
   return i + 1
 
 # Function to perform quicksort
-def quick_sort(array, low, high):
-  if low < high:
+def quick_sort(array, left, right):
+  if left < right:
  
     # Find pivot element such that
     # element smaller than pivot are on the left
     # element greater than pivot are on the right
-    pi = partition(array, low, high)
+    pivot = partition(array, left, right)
  
     # Recursive call on the left of pivot
-    quick_sort(array, low, pi - 1)
+    quick_sort(array, left, pivot - 1)
  
     # Recursive call on the right of pivot
-    quick_sort(array, pi + 1, high)
+    quick_sort(array, pivot + 1, right)
  
    
 
